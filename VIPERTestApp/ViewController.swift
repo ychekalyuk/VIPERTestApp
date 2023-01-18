e99c1869d380d5868b6d9199bf6b93240b7be4bc
Yurij Chekalyuk <yura_chekalyuk@mail.ru>
2023-01-19 01:08:47 +0300

    Initial Commit

---
+++ VIPERTestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VIPERTestApp
+//
+//  Created by Юрий Альт on 19.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
